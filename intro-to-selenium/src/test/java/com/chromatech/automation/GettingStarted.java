455753fb7b47eefe842e101d8ff16e6c366b9cbf
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-12-16 15:15:18 -0800

    Locates elements

--- intro-to-selenium/src/test/java/com/chromatech/automation/GettingStarted.java
+++ intro-to-selenium/src/test/java/com/chromatech/automation/GettingStarted.java
@@ -1,8 +1,24 @@
 package com.chromatech.automation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class GettingStarted {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().deleteAllCookies();
+        driver.get("https://chroma.mexil.it/site/login");
+
+        driver.findElement(By.id("form-username")).sendKeys("test@example.com");
+        driver.findElement(By.id("form-password")).sendKeys("12345");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
     }
 }
