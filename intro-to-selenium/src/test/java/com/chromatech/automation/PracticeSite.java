e2e34cf7cb44e594c7eabeb3c92ebca958512294
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-12-17 00:38:51 -0800

    Selecting options on practice site

--- intro-to-selenium/src/test/java/com/chromatech/automation/PracticeSite.java
+++ intro-to-selenium/src/test/java/com/chromatech/automation/PracticeSite.java
@@ -8,17 +8,42 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class PracticeSite {
 
     public static void main(String[] args) {
+        // Navigate to website
         String url = "https://chromatechacademy.net/selenium-practice/";
         WebDriver driver = new FirefoxDriver();
+        driver.manage().deleteAllCookies();
         driver.get(url);
 
-        // Locate element
-        WebElement homeButton = driver.findElement(By.xpath("/html/body/header/div/button[2]"));
-        if (homeButton == null) {
-            System.out.println("Keep looking");
-        } else {
-            System.out.println("Found it");
-        }
+        // Locate drop down button
+        WebElement dropDownButtonElement = driver.findElement(By.xpath("/html/body/div[1]/div[1]/fieldset/div/button"));
+        dropDownButtonElement.click();
+        
+        // Select option 2
+        WebElement option2Element = driver.findElement(By.xpath("/html/body/div[1]/div[1]/fieldset/div/div/label[2]/input"));
+        option2Element.click();
+        dropDownButtonElement.click();
+        
+        // Select JQuery
+        WebElement jQueryElement = driver.findElement(By.cssSelector("[value='Jquery']"));
+        jQueryElement.click();
+        
+        // Select Java
+        WebElement javaElement = driver.findElement(By.cssSelector("[value='Java']"));
+        javaElement.click();
+        
+        // Locate DropDown Multi-Select Element
+        WebElement multiSelecElement = driver.findElement(By.xpath("/html/body/div[1]/div[3]/fieldset/div/div[1]/input"));
+        multiSelecElement.click();
+        
+        // Select Java
+        // WebElement javaMultiSelectElement =
+        // driver.findElement(By.cssSelector("[aria-activedescendant='choices--choices-multiple-remove-button-item-choice-7']"));
+        // javaMultiSelectElement.click();
+        
+        // Select JQuery
+        // WebElement jQueryMultiSelectElement =
+        // driver.findElement(By.cssSelector("[aria-activedescendant='choices--choices-multiple-remove-button-item-choice-9']"));
+        // jQueryMultiSelectElement.click();
 
         try {
             Thread.sleep(5000);
