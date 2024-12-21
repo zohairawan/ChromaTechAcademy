d9f8e71de9807b006a71e39ab9b9302ab0636d62
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-12-20 23:58:48 -0800

    Practices on practice site

--- src/test/java/com/automation/GettingStarted.java
+++ src/test/java/com/automation/GettingStarted.java
@@ -1,8 +1,30 @@
 package com.automation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class GettingStarted {
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        WebDriver firefox = new FirefoxDriver();
+        String url = "https://chromatechacademy.net/selenium-practice/";
+        firefox.manage().deleteAllCookies();
+        firefox.get(url);
+
+        // Click 'Bootstrap multi-select drop-down example'
+        firefox.findElement(By.xpath("/html/body/div[1]/div[1]/fieldset/div/button")).click();
+        firefox.findElement(By.xpath("(//input[@type='checkbox'])[3]")).click();
+        firefox.findElement(By.xpath("/html/body/div[1]/div[2]/fieldset/select/option[1]")).click();
+        firefox.findElement(By.xpath("//option[@value='Jquery']")).click();
+        
+        
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        firefox.quit();
     }
 }
