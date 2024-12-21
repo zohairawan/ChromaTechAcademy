d2f949a025200549698ea91e1a76e868af5ef5cc
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-12-21 00:21:38 -0800

    More practice

--- src/test/java/com/automation/GettingStarted.java
+++ src/test/java/com/automation/GettingStarted.java
@@ -2,6 +2,7 @@ package com.automation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GettingStarted {
@@ -15,9 +16,16 @@ public class GettingStarted {
         // Click 'Bootstrap multi-select drop-down example'
         firefox.findElement(By.xpath("/html/body/div[1]/div[1]/fieldset/div/button")).click();
         firefox.findElement(By.xpath("(//input[@type='checkbox'])[3]")).click();
+        firefox.findElement(By.xpath("/html/body/div[1]/div[1]/fieldset/div/button")).click();
+        // Click 'DropDown Multi-Select Example'
         firefox.findElement(By.xpath("/html/body/div[1]/div[2]/fieldset/select/option[1]")).click();
         firefox.findElement(By.xpath("//option[@value='Jquery']")).click();
-        
+        // Click 'DropDown Multi-Select Example'
+        // firefox.findElement(By.xpath("//input[@aria-activedescendant=\"choices--choices-multiple-remove-button-item-choice-1\"]")).click();;
+        // firefox.findElement(By.xpath("//input[@aria-activedescendant=\"choices--choices-multiple-remove-button-item-choice-2\"]")).click();;
+
+        // Click 'Suggession Class Example'
+        firefox.findElement(By.xpath("//input[@id=\"autocomplete\"]")).sendKeys("TEXT INPUT");
         
         try {
             Thread.sleep(2000);
